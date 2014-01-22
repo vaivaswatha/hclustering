a4e110c79504df6b8b55743f6ab55f3d7b410ee0
Vaivaswatha N <vaivaswatha@hpc.serc.iisc.in>
2014-01-22 15:01:38 +0530

    bulk of the clustering code is done. need to write testing code still.

--- hclustering.cpp
+++ hclustering.cpp
@@ -1,41 +1,91 @@
 #include <vector>
+#include <cassert>
 #include "hclustering.h"
 
-template <class Pt>
-struct TreeNode
-{
-    Pt *el;
-    TreeNode *root;
-    std::vector<TreeNode *> members;
-    // assert ((root == this && members.size() > 0) ||
-    //         (root != this && members.size() == 0))
+#define INF -1.0
+#define IS_REP(x) (clusters[(x)].rep == &clusters[(x)])
 
-    TreeNode(void) { 
-	el = NULL;
-	root = NULL;
-    };
-};
-
-unsigned make1D(unsigned i, unsigned j, unsigned nElm)
+inline unsigned make1D(unsigned i, unsigned j, unsigned nElm)
 {
     return ((nElm * i) + j);
 }
 
-#define INF -1.0
+template <class Pt>
+float HClustering<Pt>::computeLinkage(unsigned c1, unsigned c2)
+{
+    unsigned i, j;
+    float retVal = INF;
+
+    assert(IS_REP(c1) && IS_REP(c2));
+
+    // for looping convinience, we add the rep of a node to
+    // its "members" list and pop it back later
+    clusters[c1].members.push_back(&clusters[c1]);
+    clusters[c2].members.push_back(&clusters[c2]);
+
+    switch (linkageType) {
+    case LINK_MINIMUM:
+    {
+	float minDistanceSeen = INF;
+	for (i = 0; i < clusters[c1].members.size(); i++) {
+	    for (j = 0; j < clusters[c2].members.size(); j++) {
+		float distance = distanceMatrix[make1D(clusters[c1].members[i], 
+						       clusters[c2].members[j], 
+						       clusters.size())];
+		if (minDistanceSeen == INF ||
+		    minDistanceSeen > distance)
+		    {
+			minDistanceSeen = distance;
+		    }
+	    }
+	}
+	retVal = minDistanceSeen;
+    }
+    break;
+    case LINK_MAXIMUM:
+    {
+	// abusing INF here.
+	float maxDistanceSeen = INF;
+	for (i = 0; i < clusters[c1].members.size(); i++) {
+	    for (j = 0; j < clusters[c2].members.size(); j++) {
+		float distance = distanceMatrix[make1D(clusters[c1].members[i], 
+					        clusters[c2].members[j], 
+						clusters.size())];
+		if (maxDistanceSeen == INF ||
+		    maxDistanceSeen < distance)
+		{
+		    maxDistanceSeen = distance;
+		}
+	    }
+	}
+	retVal = maxDistanceSeen;
+    }
+    break;
+    case LINK_AVERAGE:
+	assert(0 && "LINK_AVERAGE not yet supported");
+	break;
+    }
+
+    // pop back the elements artificially inserted earlier.
+    clusters[c1].members.pop_back();
+    clusters[c2].members.pop_back();
+
+    assert(retVal != INF);
+    return retVal;
+}
 
 template <class Pt>
 void HClustering<Pt>::cluster(Pt *data, unsigned nElm)
 {
-    unsigned i, j, n;
-    std::vector<TreeNode<Pt> > clusters;
-    std::vector<float> distanceMatrix;
-    std::vector<std::pair<unsigned, float> > shortestDistance (nElm+1, INF);
+    unsigned i, j, k, n;
+    std::vector<std::pair<unsigned, float> > shortestDistance (nElm, INF);
 
     // Initially there are nElm clusters, so have these many
     // TreeNodes and link them to the input data.
     clusters.resize(nElm);
     for (i = 0; i < nElm; i++) {
 	clusters[i].el = &data[i];
+	clusters[i].id = i;
     }
 
     // TODO: This can be a triangural matrix to save space.
@@ -48,22 +98,118 @@ void HClustering<Pt>::cluster(Pt *data, unsigned nElm)
 
     // compute the shortest distance array.
     for (i = 0; i < nElm; i++) {
-	for (j = i+1; j < nElm; j++) {
+	for (j = 0; j < nElm; j++) {
+	    float linkageIJ;
+	    // No need to check for cluster rep here since
+	    // at this everybody is his own rep.
+	    assert(IS_REP(i));
+	    if (i == j)
+		continue;
+	    linkageIJ = computeLinkage(i, j);
 	    if (shortestDistance[i].second == INF ||
-		distanceMatrix[make1D(i, j, nElm)] < shortestDistance[i].second)
+		linkageIJ < shortestDistance[i].second)
 	    {
 		shortestDistance[i].first = j;
-		shortestDistance[i].second = distanceMatrix[make1D(i, j, nElm)];
-		// symmetry!
-		shortestDistance[j].first = i;
-		shortestDistance[j].second = shortestDistance[i].second;
+		shortestDistance[i].second = linkageIJ;
 	    }
 	}
     }
 
     // Run the clustering loop till there are only numClusters remaining.
-    for (n = nElm; n >= numClusters; n--) {
+    for (n = nElm; n > numClusters; n--) {
+	float shortestDistanceSeen = INF;
+	unsigned shortestDistanceFrom = nElm;
+	    
+	// TODO: Keep a list of elements that represent a cluster.
+	// TODO: Maybe some kind of sorted list? I don't know
+	for (i = 0; i < nElm; i++) {
+	    // If this element is not the rep for the cluster it is in:
+	    if (!IS_REP(i))
+		continue;
+	    if (shortestDistanceSeen == INF ||
+		shortestDistanceSeen > shortestDistance[i].second)
+	    {
+		shortestDistanceSeen = shortestDistance[i].second;
+		shortestDistanceFrom = shortestDistance[i].first;
+	    }
+	}
+
+	assert (shortestDistanceSeen != INF && shortestDistanceFrom < nElm);
+	i = shortestDistanceFrom;
+	j = shortestDistance[i].first;
+	assert(i != j);
+	// i and j must both represent their clusters already
+	// (since we only look at reps during during each iteration).
+	assert(IS_REP(i) && IS_REP(j));
+
+	if (clusters[i].members.size() < clusters[j].members.size()) {
+	    // let 'j' represent the smaller cluster.
+	    unsigned temp = i;
+	    i = j;
+	    j = temp;
+	}
+	// We now need to combine the clusters i and j,
+	// So make 'j' belong to 'i' as it is smaller.
+	clusters[i].members.insert(clusters[i].members.end(), 
+				   clusters[j].members.begin(),
+				   clusters[j].members.end());
+	clusters[j].rep = &clusters[i];
+	{
+	    // Just to free up memory used for the members array.
+	    std::vector<TreeNode *> tmp;
+	    clusters[j].members.swap(tmp);
+	}
 	
+	// compute linkage between clusters (w.r.t i).
+	for (k = 0; k < nElm; k++) {
+	    float distance;
+
+	    // ignore non rep data. also ignore i itself.
+	    if (!IS_REP(k) || k == i)
+		continue;
+	    distance = computeLinkage(i, k);
+	    assert(distance != INF);
+	    // Now update the shortestDistance array.
+	    if (shortestDistance[i].second > distance) {
+		shortestDistance[i].second = distance;
+		shortestDistance[i].first = k;
+	    }
+	    // for 'k', the shortestDistance array may be
+	    // pointing to a non-rep now. Correct that.
+	    if (!IS_REP(shortestDistance[k].first)) {
+		// This must have just happened.
+		assert(clusters[shortestDistance[k].first].rep ==
+		       &clusters[i]);
+		if (linkageType == LINK_MINIMUM) {
+		    // In case of LINK_MINIMUM the nearest
+		    // cluster would now be 'i' (since 'j'
+		    // now belongs to 'i').
+		    assert(shortestDistance[k].second == distance);
+		    shortestDistance[k].first = i;
+		} else if (distance > shortestDistance[k].second) {
+		    // NOTE: This can never happen in LINK_MINIMUM.
+		    // Will now need to look through ALL
+		    // clusters for a new nearest linkage.
+		    for (j = 0; j < nElm; j++) {
+			float linkageKJ;
+			if (j == k || !IS_REP(j))
+			    continue;
+			linkageKJ = computeLinkage(k, j);
+			if (shortestDistance[k].second == INF ||
+			    linkageKJ < shortestDistance[k].second)
+			{
+				shortestDistance[k].first = j;
+				shortestDistance[k].second = linkageKJ;
+			}
+		    }
+		}
+		// computeLinkage(i, k) cannot be smaller than
+		// the known shortest distance for node k
+		assert(distance >= shortestDistance[k].second);
+		// At this point, the shortest distance must be to a rep.
+		assert(IS_REP(clusters[k].first));
+	    }
+	}
     }
 }
 
@@ -71,6 +217,5 @@ void HClustering<Pt>::cluster(Pt *data, unsigned nElm)
 int main (void)
 {
     
-
 }
 #endif // TEST_CODE
--- hclustering.h
+++ hclustering.h
@@ -8,18 +8,49 @@ class HClustering
 {
  public:
     typedef float (*DistFunc)(Pt &el1, Pt &el2);
+    enum LinkageType {
+	LINK_MINIMUM,
+	LINK_MAXIMUM,
+	LINK_AVERAGE,
+    };
 
-    HClustering(DistFunc p_distFunc, unsigned p_numClusters) {
+    HClustering(DistFunc p_distFunc, unsigned p_numClusters, 
+		LinkageType p_linkageType = LINK_MINIMUM) 
+    {
 	distFunc = p_distFunc;
 	numClusters = p_numClusters;
+	linkageType = p_linkageType;
     }
 
     void cluster(Pt *data, unsigned nElm);
 
  private:
+
+    struct TreeNode
+    {
+	Pt *el;
+	unsigned id;
+	TreeNode *root;
+	std::vector<TreeNode *> members;
+	// assert ((root == this && members.size() > 0) ||
+	//         (root != this && members.size() == 0))
+
+	TreeNode(void) { 
+	    el = NULL;
+	    // Initially every node is a cluster by itself.
+	    root = this;
+	};
+    };
+
     DistFunc distFunc;
     unsigned numClusters;
+    LinkageType linkageType;
+    std::vector<TreeNode> clusters;
+    std::vector<float> distanceMatrix;
+
     HClustering(void) { ; };
+    // compute linkage between clusters i and k.
+    float computeLinkage(unsigned i, unsigned k);
 };
 
 #endif // _HCLUSTERING_
